c82283213e77333c23516c494d8baf42a4f6fcb9
Bryce_Xu <bryce_xu@outlook.com>
2023-02-17 15:51:49 -0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from streamlit_chat import message
 
-def generate_response(prompt):
-    message = "This is a default answer."
-    return message
+def generate_response(question):
+    print(question)
+    """
+    Run our system here to generate an answer
+    
+    answer = system.generate_answer(question)
+    
+    """
+    answer = "This is a default answer."
+    return answer
 
 st.title("BNY Mellon Chatbot Project")
 
@@ -13,16 +20,21 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
-    return input_text
+if 'submit' not in st.session_state:
+    st.session_state.submit = ''
 
-user_input = get_text()
+if 'widget' not in st.session_state:
+    st.session_state.widget = ''
 
-if user_input:
+def submit():
+    st.session_state.submit = st.session_state.widget
+    st.session_state.widget = ''
+    user_input = st.session_state.submit
     output = generate_response(user_input)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
+
+input_text = st.text_input("Ask a question: ","", key="widget", on_change=submit)
 
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated']) - 1, -1, -1):
